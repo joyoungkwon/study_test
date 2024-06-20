cbd56d4e6ebc9d9b4842ba3ed7add95073ce7017
YeoungKwon <dudrnjs25602daum.net>
2024-06-20 23:06:57 +0900

    알고리즘 강의 1번째 도형보며 코드로 짜보기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 18 + 15 + 12 + 9 + 6 + 3
+        // sum 합계 구하기
+
+
+        int k = 18;
+        int sum = 0;
+
+        for(int i = k; i >= 3; i--){
+
+            sum+=i;
+
+        }
+
+        System.out.println("sum = " + sum);
+
+
     }
+
+
 }
